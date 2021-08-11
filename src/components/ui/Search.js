8449331b8e6aec3577fef93d6a8ce1a7adc011c9
Kamila <gumkowskakamila@gmail.com>
2021-08-11 20:53:43 +0200

    display cards with all pieces of info and search bar

---
+++ src/components/ui/Search.js
@@ -0,0 +1,20 @@
+import React, { useState} from 'react'
+
+
+const Search = () => {
+    return (
+        <section className='search'>
+            <form>
+                <input 
+                type='text' 
+                className='form-control' placeholder='Search characters'
+                autoFocus
+                />
+
+            </form>
+            
+        </section>
+    )
+}
+
+export default Search
